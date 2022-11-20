4c7799d684566b7d6c71977df40236b718b85044
ducnv56 <ducnv56.hrc@gmail.com>
2022-11-20 22:58:22 +0700

    Initial commit

--- app.py
+++ app.py
@@ -20,11 +20,28 @@ def index():
 
     return render_template('base.html', todo_list=todo_list)
 
+@app.route("/add", methods=["POST"])
 def add():
     # add new item
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # update existed item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # delete existed item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
 
